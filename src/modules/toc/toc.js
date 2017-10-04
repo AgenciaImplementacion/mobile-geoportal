077eb60aea7cd3f8158ab2723b134883db4aee78
Jorge Useche <naturalmentejorge@gmail.com>
2017-10-04 14:39:18 -0500

    Ejemplo de cómo agregar CSS específico.

--- src/modules/toc/toc.js
+++ src/modules/toc/toc.js
@@ -1,3 +1,6 @@
+require('./toc.css');
+
+
 // http://2ality.com/2015/01/template-strings-html.html
 
 function htmlEscape(str) {
@@ -14,39 +17,75 @@ var people = [
     { first: 'Lars', last: 'Croft', phones: ['234234234', '522343434'] }
 ];
 
+/**
+ * Muestra en una plantilla un UL
+ */
 const template = addrs => `
-    <ul>
-    ${addrs.map(addr => `
-        <li>
-          <b>${addr.first + ' ' + addr.last}</b>
-          <ul>
-            ${addr.phones.map(phone => `
-              <li>${phone}</li>
-            `).join('')}
-          </ul>
-        </li>
-    `).join('')}
-    </ul>
+  Ejemplo 1:
+  <ul>
+  ${addrs.map(addr => `
+      <li>
+        <b>${addr.first + ' ' + addr.last}</b>
+        <ul>
+          ${addr.phones.map(phone => `
+            <li>${phone}</li>
+          `).join('')}
+        </ul>
+      </li>
+  `).join('')}
+  </ul>
 `;
 
+/**
+ * Muestra en una plantilla un UL
+ * escapando los caracteres HTML del telefono
+ */
 const template2 = addrs => `
-    <ul>
-    ${addrs.map(addr => `
-        <li>
-          <b>${addr.first + ' ' + addr.last}</b>
-          <ul>
-            ${addr.phones.map(phone => `
-              <li>${htmlEscape(phone)}</li>
-            `).join('')}
-          </ul>
-        </li>
-    `).join('')}
-    </ul>
+  Ejemplo 2:
+  <ul>
+  ${addrs.map(addr => `
+      <li>
+        <b>${addr.first + ' ' + addr.last}</b>
+        <ul>
+          ${addr.phones.map(phone => `
+            <li>${htmlEscape(phone)}</li>
+          `).join('')}
+        </ul>
+      </li>
+  `).join('')}
+  </ul>
 `;
+
+
+/**
+ * Muestra en una plantilla un UL
+ * escapando los telefonos que no son números
+ */
+const template3 = addrs => `
+  Ejemplo 3:
+  <ul>
+  ${addrs.map(addr => `
+      <li>
+        <b>${addr.first + ' ' + addr.last}</b>
+        <ul>
+          ${addr.phones.filter(phone => Number(phone) + '' === phone).map(phone => `
+            <li>${htmlEscape(phone)}</li>
+          `).join('')}
+        </ul>
+      </li>
+  `).join('')}
+  </ul>
+`;
+
 
 var rendered = template(people);
 $('#contenido-barra-lateral').append(rendered);
 console.log(people, rendered);
+
 rendered = template2(people);
+$('#contenido-barra-lateral').append(rendered);
+console.log(people, rendered);
+
+rendered = template3(people);
 $('#contenido-barra-lateral').append(rendered);
 console.log(people, rendered);
